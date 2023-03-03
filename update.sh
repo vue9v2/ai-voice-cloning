5487c28683d6cadc8d32e1ae013724d1d8c90b35
lightmare <lightmare@noreply.localhost>
2023-03-03 18:34:49 +0000

    Added optional whispercpp update functionality

--- update.sh
+++ update.sh
@@ -2,4 +2,6 @@
 git pull
 git submodule update --remote
 
+if python -m pip show whispercpp &>/dev/null; then python -m pip install -U git+https://git.ecker.tech/lightmare/whispercpp.py; fi
+
 deactivate
