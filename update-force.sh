da0af4c498b69455d96e43483f37b8cecc031208
mrq <barry.quiggles@protonmail.com>
2023-03-06 16:47:34 +0000

    one more

--- update-force.sh
+++ update-force.sh
@@ -9,9 +9,9 @@ python3 -m venv venv
 source ./venv/bin/activate
 
 python3 -m pip install --upgrade pip
-python3 -m pip install -r ./dlas/requirements.txt
+python3 -m pip install -r ./requirements.txt
 python3 -m pip install -r ./tortoise-tts/requirements.txt
 python3 -m pip install -e ./tortoise-tts 
-python3 -m pip install -r ./requirements.txt
+python3 -m pip install -r ./dlas/requirements.txt
 
 deactivate
